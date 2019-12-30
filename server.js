c8cf87620a6848d72b9508f2b33295b58c1c2c37
anniepineda <anniepineda@outlook.com>
2019-12-30 12:19:05 -0800

    grid box

--- server.js
+++ server.js
@@ -6,12 +6,18 @@ const PORT = process.env.PORT || 3000;
 
 require('dotenv').config();
 
+app.use(express.static('./public'));
+
 app.set('view engine', 'ejs');
 app.set('views', './views/pages');
 
 app.get('/', (request, response) => {
   response.render('index');
 });
+
+app.get('/results', (request, response) => {
+  response.render('results');
+})
 
 //Constructor
 
