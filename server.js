b63def623e47a53fda5a5d23145069f48f907f65
Jerome Joof <jeromejoof@yahoo.com>
2019-12-31 10:01:00 -0800

    search by company is now workling

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const PORT = process.env.PORT || 3000;
 
 require('dotenv').config();
 
+
+app.use(express.static('./public'));
+
 //Middleware
 app.use(express.urlencoded({extended: true}));
 
@@ -28,7 +31,9 @@ function newSearch(request, response){
 
   let searchStr = request.body.search;
   let searchType = request.body.search;
-  let companyURL = `https://financialmodelingprep.com/api/v3/company/profile/AAPL`;
+  let companyURL = `https://financialmodelingprep.com/api/v3/company/profile/${searchStr}`;
+  // let companyURL = `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=BA&apikey=ORAVID4KP25O67E4`
+  // let companyURL = `https://financialmodelingprep.com/api/v3/company/stock/list`;
 
 
   if(searchType === 'search'){
@@ -38,17 +43,24 @@ function newSearch(request, response){
 
   superagent.get(companyURL)
     .then(result => {
+
       // console.log('this', result)
       const parseResult = JSON.parse(result.text);
-      console.log('this 2', parseResult.profile)
+      console.log('this 2', parseResult)
 
 
-      // let companyData = parseResult.body.map(data => new Company(data))
+      // let company = result.body.symbolsList.map(data => new Company(data))
+      // let regex = regex.search('\d+|$', company).group();
+      // console.log('here', regex
+
       let parseResultProfile = parseResult.profile;
-      console.log('what', parseResultProfile)
+      // let company = Object.entries(parseResultProfile).map(data => new Company(data));
+      // console.log('what', company)
       let company = new Company(parseResultProfile)
+      // let company = new Company(companyData)
+
       console.log('this 3', company)
-      console.log('this 4', company)
+      // console.log('this 4', company)
       response.render('searches/show', {company});
     })
     .catch(err => console.log(err));
@@ -57,6 +69,11 @@ function newSearch(request, response){
 function search(request, response){
   response.render('index')
 }
+
+app.get('/results', (request, response) => {
+  // console.log(mockData.symbol, mockData.profile.price)
+  response.render('results');
+})
 
 //Constructor
 function Company(obj){
@@ -68,6 +85,19 @@ function Company(obj){
   this.description = obj.description;
   this.image = obj.image;
 }
+
+// function Company(obj){
+//   this.symbol = obj.symbol;
+//   this.name = obj.name;
+//   this.equity = obj.equity;
+//   this.region = obj.region;
+// }
+
+// function Company(obj){
+//   this.symbol = obj.symbol;
+//   this.name = obj.name;
+//   this.price = obj.price;
+// }
 
 function errorHandler(request, response){
   if(response) response.status(500).render('error');
