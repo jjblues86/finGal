16267f6ed00b871dac7d18755aecf3f336c9453c
Sergey Voytov <sergvoytov@gmail.com>
2019-12-31 11:11:49 -0800

    foo

--- server.js
+++ server.js
@@ -32,7 +32,6 @@ function newSearch(request, response) {
   // response.render('index.ejs')
 
   let searchStr = request.body.search;
-
   // let searchType = request.body.search;
 
   console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>searchStr', searchStr);
@@ -59,50 +58,21 @@ function newSearch(request, response) {
     })
 }
 
-  let searchType = request.body.search;
-  let companyURL = `https://financialmodelingprep.com/api/v3/company/profile/${searchStr}`;
-  // let companyURL = `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=BA&apikey=ORAVID4KP25O67E4`
-  // let companyURL = `https://financialmodelingprep.com/api/v3/company/stock/list`;
 // console.log('x', x);
+
+
 // if (searchType === 'search') {
 //   companyURL += `insearch:${searchStr}`
 // }
 // console.log('this 1', searchType)
 
-  superagent.get(companyURL)
-    .then(result => {
-
-      // console.log('this', result)
-      const parseResult = JSON.parse(result.text);
-      console.log('this 2', parseResult)
-
-
-
-      // let company = result.body.symbolsList.map(data => new Company(data))
-      // let regex = regex.search('\d+|$', company).group();
-      // console.log('here', regex
 
 function search(request, response) {
-=======
-      let parseResultProfile = parseResult.profile;
-      // let company = Object.entries(parseResultProfile).map(data => new Company(data));
-      // console.log('what', company)
-      let company = new Company(parseResultProfile)
-      // let company = new Company(companyData)
-
-      console.log('this 3', company)
-      // console.log('this 4', company)
-      response.render('searches/show', {company});
-    })
-    .catch(err => console.log(err));
-}
-
-function search(request, response){
   response.render('index')
 }
 
 app.get('/results', (request, response) => {
-  // console.log(mockData.symbol, mockData.profile.price)
+  console.log(mockData.symbol, mockData.profile.price)
   response.render('results');
 })
 
@@ -112,7 +82,7 @@ function Company(obj) {
   this.symbol = obj.symbol;
   this.price = obj.price;
   this.sector = obj.sector;
-  this.beta = obj.beta;
+  this.ceo = obj.ceo;
   this.description = obj.description;
   this.image = obj.image;
 }
@@ -137,22 +107,12 @@ function searchAlpha(userKey){
     .catch(error => {
       console.error('catch on it ', error)
     })
-=======
-// function Company(obj){
-//   this.symbol = obj.symbol;
-//   this.name = obj.name;
-//   this.equity = obj.equity;
-//   this.region = obj.region;
-// }
-
-// function Company(obj){
-//   this.symbol = obj.symbol;
-//   this.name = obj.name;
-//   this.price = obj.price;
-// }
-
-function errorHandler(request, response){
-  if(response) response.status(500).render('error');
 }
+
+
+
+
+
+
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
