459512e27acf8fc883d4dbc16801662ec041bf27
anniepineda <anniepineda@outlook.com>
2019-12-31 13:46:12 -0800

    homepage

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const superagent = require('superagent');
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 require('dotenv').config();
 
