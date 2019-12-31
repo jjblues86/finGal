19200bc00f2856cae9462c8d7f914c860cee8166
Sergey Voytov <sergvoytov@gmail.com>
2019-12-31 09:58:13 -0800

    working 2 apis chained together

--- server.js
+++ server.js
@@ -32,37 +32,40 @@ function newSearch(request, response) {
   // response.render('index.ejs')
 
   let searchStr = request.body.search;
-  let searchType = request.body.search;
+  // let searchType = request.body.search;
 
-console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>searchStr', searchStr);
-
-
-
-  let companyURL = `https://financialmodelingprep.com/api/v3/company/profile/AAPL`;
-
-
-  // if (searchType === 'search') {
-  //   companyURL += `insearch:${searchStr}`
-  // }
-  console.log('this 1', searchType)
-
-  superagent.get(companyURL)
-    .then(result => {
-      // console.log('this', result)
-      const parseResult = JSON.parse(result.text);
-      console.log('this 2', parseResult.profile)
+  console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>searchStr', searchStr);
+  searchAlpha(searchStr)
+    .then(result =>{
+      console.log('this 12', result)
+      let companyURL = `https://financialmodelingprep.com/api/v3/company/profile/${result}`;
+      superagent.get(companyURL)
+        .then(result => {
+          // console.log('this', result)
+          const parseResult = JSON.parse(result.text);
+          console.log('this 2', parseResult.profile)
 
 
-      // let companyData = parseResult.body.map(data => new Company(data))
-      let parseResultProfile = parseResult.profile;
-      console.log('what', parseResultProfile)
-      let company = new Company(parseResultProfile)
-      console.log('this 3', company)
-      console.log('this 4', company)
-      response.render('searches/show', { company });
+          // let companyData = parseResult.body.map(data => new Company(data))
+          let parseResultProfile = parseResult.profile;
+          console.log('what', parseResultProfile)
+          let company = new Company(parseResultProfile)
+          console.log('this 3', company)
+          console.log('this 4', company)
+          response.render('searches/show', { company });
+        })
+        .catch(err => console.log(err));
     })
-    .catch(err => console.log(err));
 }
+
+// console.log('x', x);
+
+
+// if (searchType === 'search') {
+//   companyURL += `insearch:${searchStr}`
+// }
+// console.log('this 1', searchType)
+
 
 function search(request, response) {
   response.render('index')
@@ -85,23 +88,26 @@ function Company(obj) {
 }
 
 // logic to pull sticker information from the company name to send to the main API
-const userKey = 'facebook';
 
-superagent.get(`https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${userKey}&apikey=7R6ONK4007JF3LU7`).then(response => {
+function searchAlpha(userKey){
 
-  let stickerObject = response.body.bestMatches[0];
+  return superagent.get(`https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${userKey}&apikey=7R6ONK4007JF3LU7`).then(response => {
 
-  let symbolShare = stickerObject['1. symbol'];
+    let stickerObject = response.body.bestMatches[0];
 
-  let nameShare = stickerObject['2. name'];
-  console.log('data to parse to next API', symbolShare, nameShare);
+    let symbolShare = stickerObject['1. symbol'];
 
+    let nameShare = stickerObject['2. name'];
+    console.log('data to parse to next API', symbolShare, nameShare);
 
-})
+    return symbolShare;
 
-  .catch(error => {
-    console.error('catch on it ', error)
   })
+
+    .catch(error => {
+      console.error('catch on it ', error)
+    })
+}
 
 
 
