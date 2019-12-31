b22f2c21d5ef8adc451c05c61873dd3584b860b9
Martin Papa <martin.papa@outlook.com>
2019-12-30 16:05:24 -0800

    did some style, need constructors from the back end team

--- server.js
+++ server.js
@@ -1,4 +1,26 @@
 'use strict';
+let mockData = {
+  "symbol" : "AAPL",
+  "profile" : {
+    "price" : 291.45,
+    "beta" : "1.139593",
+    "volAvg" : "36724977",
+    "mktCap" : "1345844630130.00",
+    "lastDiv" : "2.92",
+    "range" : "142-233.47",
+    "changes" : -0.75,
+    "changesPercentage" : "(-0.25%)",
+    "companyName" : "Apple Inc.",
+    "exchange" : "Nasdaq Global Select",
+    "industry" : "Computer Hardware",
+    "website" : "http://www.apple.com",
+    "description" : "Apple Inc is designs, manufactures and markets mobile communication and media devices and personal computers, and sells a variety of related software, services, accessories, networking solutions and third-party digital content and applications.",
+    "ceo" : "Timothy D. Cook",
+    "sector" : "Technology",
+    "image" : "https://financialmodelingprep.com/images-New-jpg/AAPL.jpg"
+  }
+};
+
 const express = require('express');
 const app = express();
 
@@ -16,6 +38,7 @@ app.get('/', (request, response) => {
 });
 
 app.get('/results', (request, response) => {
+  console.log(mockData.symbol, mockData.profile.price)
   response.render('results');
 })
 
