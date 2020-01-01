7300f5eb4e6160cb0d3e1f68bb7d7cd3908b26fa
Sergey Voytov <sergvoytov@gmail.com>
2019-12-31 16:35:54 -0800

    events are working

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ require('dotenv').config();
 app.use(express.static('./public'));
 
 //Middleware
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
 app.set('views', './views/pages');
@@ -21,20 +21,20 @@ app.get('/', search);
 app.post('/searches', newSearch);
 
 
-function search(request, response){
+function search(request, response) {
   response.render('index')
 }
 
-function newSearch(request, response){
+function newSearch(request, response) {
 
   // let searchType = request.body.search;
   let searchStr = request.body.search[0];
   let searchType = request.body.search[1];
   let companyURL = `https://financialmodelingprep.com/api/v3/company/profile/${searchStr}`;
 
-  if(searchType === 'company'){
+  if (searchType === 'company') {
     let companySearch = searchAlpha(searchStr);
-    companySearch.then( result => {
+    companySearch.then(result => {
       // console.log('this 0', result)
       companyURL = `https://financialmodelingprep.com/api/v3/company/profile/${result}`;
       superagent.get(companyURL)
@@ -47,7 +47,7 @@ function newSearch(request, response){
           let company = new Company(parseResultProfile)
           // console.log('this 3', company)
           // console.log('this 4', company)
-          response.render('searches/show', {company});
+          response.render('searches/show', { company });
         })
         .catch(err => console.log(err));
 
@@ -55,7 +55,7 @@ function newSearch(request, response){
   }
   // console.log('this 1', searchType)
 
-  console.log('BACON',companyURL);
+  console.log('BACON', companyURL);
   superagent.get(companyURL)
     .then(resultData => {
 
@@ -66,7 +66,7 @@ function newSearch(request, response){
       let company = new Company(parseResultProfile)
       // console.log('this 3', company)
       // console.log('this 4', company)
-      response.render('searches/show', {company});
+      response.render('searches/show', { company });
     })
     .catch(err => console.log(err));
   //////////////
@@ -74,7 +74,7 @@ function newSearch(request, response){
 /////////////////
 
 // logic to pull sticker information from the company name to send to the main API
-function searchAlpha(userKey){
+function searchAlpha(userKey) {
 
   return superagent.get(`https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${userKey}&apikey=7R6ONK4007JF3LU7`).then(response => {
 
@@ -100,7 +100,7 @@ function searchAlpha(userKey){
 // })
 
 //Constructor
-function Company(obj){
+function Company(obj) {
   this.name = obj.companyName;
   this.symbol = obj.symbol;
   this.price = obj.price;
@@ -114,7 +114,7 @@ function Company(obj){
 app.get('/books', (req, res) => {
   superagent.get(`https://www.googleapis.com/books/v1/volumes?q=finance`).then(data => {
     const booksArray = data.body.items.map(book => new Book(book));
-    const books= booksArray.slice(0, 3);
+    const books = booksArray.slice(0, 3);
     res.render('books', { books });
   }).catch(error => {
     res.render('error', { error });
@@ -128,8 +128,41 @@ function Book(bookObj) {
   this.link = bookObj.volumeInfo.previewLink;
 }
 
-function errorHandler(request, response){
-  if(response) response.status(500).render('error');
+function errorHandler(request, response) {
+  if (response) response.status(500).render('error');
 }
+
+
+
+app.get('/event', (req, res) => {
+  console.log('data')
+  superagent.get(`http://api.eventful.com/json/events/search?q=investing&where=Seattle&within=25&app_key=5DsQwPWqNz4zHmtM`).then(data => {
+
+  let parsedData= JSON.parse(data.text);
+
+    // let events = data.events.event[0].title;
+    // console.log('data afetr data', JSON.parse(data.text))
+    const eventsArray = parsedData.events.event.map(event => new Event(event));
+    const events = eventsArray.slice(0, 3);
+    // console.log('event', events)
+    res.render('event', { events });
+
+
+  }).catch(error => {
+    console.log(error)
+    res.render('error', { error });
+  });
+});
+
+
+//event constructor
+
+function Event(eventObj) {
+  this.title = eventObj.title,
+    this.city = eventObj.city_name,
+    this.start_time = eventObj.start_time
+  // this.summary = summary
+}
+
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
