f7df60b37613edff779f1fa58ad347fe09e79104
Martin Papa <martin.papa@outlook.com>
2020-01-02 14:56:01 -0800

    finished displaying books and event, and news api

--- server.js
+++ server.js
@@ -16,24 +16,24 @@ app.set('view engine', 'ejs');
 app.set('views', './views/pages')
 
 //Routes
-app.get('/', search);
+// app.get('/', search);
 app.post('/results', newSearch);
 
 
 
-function search(request, response) {
-  response.render('index')
-}
+// function search(request, response) {
+//   response.render('index')
+// }
 
 //Search functionality for users to choose from company name/symbol
-function newSearch(request, response){
+function newSearch(request, response) {
 
   let searchStr = request.body.search[0];
   let searchType = request.body.search[1];
   let companyURL = `https://financialmodelingprep.com/api/v3/company/profile/${searchStr}`;
   if (searchType === 'company') {
     let companySearch = searchAlpha(searchStr);
-    companySearch.then( result => {
+    companySearch.then(result => {
 
       companyURL = `https://financialmodelingprep.com/api/v3/company/profile/${result}`;
       superagent.get(companyURL)
@@ -43,7 +43,7 @@ function newSearch(request, response){
           let parseResultProfile = parseResult.profile;
           let company = new Company(parseResultProfile)
 
-          response.render('results', {company});
+          response.render('results', { company });
 
         })
         .catch(err => console.log(err));
@@ -56,18 +56,22 @@ function newSearch(request, response){
       let parseResultProfile = parseResult.profile;
       let company = new Company(parseResultProfile)
       // getStockNews(searchStr);
-      response.render('results', {company});
+
+      getStockNews(searchStr).then(news => {
+        const newsArray = news.map(news => new News(news));
+        response.render('results', { newsArray, company });
+      });
+
+      // response.render('results', { newsArray });
     }).catch(err => console.log(err));
 
-    getStockNews(searchStr).then(news => {
-    const newsArray = news.map(news => new News(news));
-    response.render('results', { newsArray });
-    });
-    // console.log('222222222222222', getStockNews(searchStr).then(news => {
-    //   const newsArray = news.body.data.map(news => new News(news));
-    //   console.log('kkkkkkkkkkkk',news.body);
-    //   response.render('results', { newsArray });
-    // }));
+
+
+  // console.log('222222222222222', getStockNews(searchStr).then(news => {
+  //   const newsArray = news.body.data.map(news => new News(news));
+  //   console.log('kkkkkkkkkkkk',news.body);
+  //   response.render('results', { newsArray });
+  // }));
   // getStockNews(searchStr).then(news => {
   //   // const newsArray = news.body.data.map(news => new News(news));
   //   // console.log('kkkkkkkkkkkk',news.body);
@@ -77,18 +81,18 @@ function newSearch(request, response){
 // Get News from API using a ticker
 function getStockNews(ticker) {
   let newsURL = `https://stocknewsapi.com/api/v1?tickers=${ticker}&items=5&token=${process.env.STOCK_NEWS_API}`;
-  return superagent.get(newsURL).then( data => {
+  return superagent.get(newsURL).then(data => {
     // const newsArray = data.body.data.map(news => new News(news));
     let newsObj = data.body.data;
     // res.render('results', { newsArray });
     return newsObj;
   }).catch(error => {
-    console.err('error', error);
+    console.error('error', error);
   });
 }
 
 //logic to pull sticker information from the company name to send to the main API
-function searchAlpha(userKey){
+function searchAlpha(userKey) {
 
   return superagent.get(`https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${userKey}&apikey=7R6ONK4007JF3LU7`).then(response => {
 
@@ -97,7 +101,7 @@ function searchAlpha(userKey){
     let symbolShare = stickerObject['1. symbol'];
 
     let nameShare = stickerObject['2. name'];
-    console.log('data to parse to next API', symbolShare, nameShare);
+    // console.log('data to parse to next API', symbolShare, nameShare);
 
     return symbolShare;
 
@@ -121,7 +125,7 @@ function searchAlpha(userKey){
 
 
 //Company Constructor
-function Company(obj){
+function Company(obj) {
   this.name = obj.companyName;
   this.symbol = obj.symbol;
   this.price = obj.price;
@@ -135,29 +139,29 @@ app.get('/', (req, res) => {
   superagent.get(`https://www.googleapis.com/books/v1/volumes?q=finance`).then(data => {
     const booksArray = data.body.items.map(book => new Book(book));
     const books = booksArray.slice(0, 3);
-    res.render('index', { books });
-  }).catch(error => {
-    res.render('error', { error });
+
+    superagent.get(`http://api.eventful.com/json/events/search?q=investing&where=Seattle&within=25&app_key=5DsQwPWqNz4zHmtM`).then(data => {
+
+      let parsedData = JSON.parse(data.text);
+      // let events = data.events.event[0].title;
+      // console.log('data afetr data', JSON.parse(data.text))
+      const eventsArray = parsedData.events.event.map(event => new Event(event));
+
+      const events = eventsArray.slice(0, 3);
+      res.render('index', { books, events });
+    }).catch(error => {
+      res.render('error', { error });
+    });
   });
-});
 
-app.get('/event', (req, res) => {
-  superagent.get(`http://api.eventful.com/json/events/search?q=investing&where=Seattle&within=25&app_key=5DsQwPWqNz4zHmtM`).then(data => {
+  // app.get('/event', (req, res) => {
 
-    let parsedData= JSON.parse(data.text);
-    // let events = data.events.event[0].title;
-    // console.log('data afetr data', JSON.parse(data.text))
-    const eventsArray = parsedData.events.event.map(event => new Event(event));
-
-    const events = eventsArray.slice(0, 3);
-    // console.log('event', events)
-    res.render('event', { events });
-
-
-  }).catch(error => {
-    console.log(error)
-    res.render('error', { error });
-  });
+  //   // console.log('event', events)
+  //   res.render('event', { events });
+  // }).catch(error => {
+  //   console.log(error)
+  //   res.render('error', { error });
+  // });
 });
 
 // app.get('/results', (req, res) => {
@@ -182,8 +186,8 @@ function Book(bookObj) {
 //event constructor
 function Event(eventObj) {
   this.title = eventObj.title,
-  this.city = eventObj.city_name,
-  this.start_time = eventObj.start_time
+    this.city = eventObj.city_name,
+    this.start_time = eventObj.start_time
 }
 
 // News Constructor
@@ -195,8 +199,8 @@ function News(newsObj) {
   this.imageUrl = newsObj.image_url;
 }
 
-function errorHandler(request, response){
-  if(response) response.status(500).render('error');
+function errorHandler(request, response) {
+  if (response) response.status(500).render('error');
 
 }
 
